331ca0ee50ff1fa95e48548c5736cca996f29dd5
SukkaW <isukkaw@gmail.com>
2019-11-12 18:16:26 +0800

    str: bring up page.content & toc

--- source/theme-doku/js/doku.js
+++ source/theme-doku/js/doku.js
@@ -1,3 +1,33 @@
-(function(window, document) {
+(function (window, document) {
+    /* Auto fixed */
+    function autoFixed(targetElID, relativeElID, navbarElID) {
+        var navbarHeight;
+        var relativeHeight = document.getElementById(relativeElID).clientHeight;
+        var target = document.getElementById(targetElID);
+
+        if (navbarElID) {
+            navbarHeight = document.getElementById(navbarElID).clientHeight;
+            relativeHeight += navbarHeight;
+        };
+
+        function updateSidebarPosition() {
+            var scrollTop = document.scrollingElement.scrollTop;
+
+            if (scrollTop > relativeHeight) {
+                target.classList.add('doku-fixed');
+            } else {
+                target.classList.remove('doku-fixed');
+            }
+        }
+
+        window.addEventListener('scroll', function () {
+            window.requestAnimationFrame(updateSidebarPosition);
+        });
+
+        updateSidebarPosition();
+    };
+
+    autoFixed('doku-sidebar', 'doku-navbar');
+    /* Copyright year auto update */
     document.getElementById('doku-copyrght-year').textContent = new Date().getFullYear();
 })(window, document);
